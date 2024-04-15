221f0594fcc50d51afd8d3d236f4018387cc9c69
AbdMzn <92449417+AbdMzn@users.noreply.github.com>
2024-04-15 12:27:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ document_name = "data"
 countries = open("countries.txt", "r").read().split("\n")
 populations = open("population.txt", "r").read().split("\n")
 
-""" for i in range(len(countries)):
-    pull_worldometer_data(countries[i]) """
+for i in range(len(countries)):
+    pull_worldometer_data(countries[i])
 
 with open(f'{document_name}.csv', 'a', newline='') as file:
     writer = csv.writer(file)
